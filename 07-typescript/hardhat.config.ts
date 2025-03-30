1498ad2e56b885f2e96fdfb6d6a6c1577f07c270
A-STAR <andreysuperstar@gmail.com>
2025-03-30 04:48:29 +0300

    feat(typesscript): set up TypeScript

---
+++ 07-typescript/hardhat.config.ts
@@ -0,0 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+};
+
+export default config;
