c2c1ff6f0817f6356faa1b9b95ee95f04e40da02
A-STAR <andreysuperstar@gmail.com>
2025-04-07 17:23:46 +0300

    feat(plugins): add request task

--- 12-plugins/hardhat.config.ts
+++ 12-plugins/hardhat.config.ts
@@ -1,5 +1,6 @@
-import { HardhatUserConfig, extendEnvironment, task } from "hardhat/config";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { HardhatUserConfig, extendEnvironment, extendProvider, task } from "hardhat/config";
+import { EIP1193Provider, HardhatConfig, HardhatRuntimeEnvironment, RequestArguments } from "hardhat/types";
+import { ProviderWrapper } from 'hardhat/plugins'
 import "@nomicfoundation/hardhat-toolbox";
 
 extendEnvironment((hre) => {
@@ -8,6 +9,41 @@ extendEnvironment((hre) => {
 
 task("envtest", async (args, hre) => {
   console.log((hre as HardhatRuntimeEnvironment & { hi: string }).hi);
+});
+
+
+class FixedGasProvider extends ProviderWrapper {
+  constructor(
+    public readonly gasPrice: bigint,
+    protected readonly _wrappedProvider: EIP1193Provider
+  ) {
+    super(_wrappedProvider);
+  }
+
+  public async request(args: RequestArguments) {
+    if (args.method === "eth_estimateGas") {
+      return this.gasPrice;
+    } else if (args.method === "eth_sendTransaction") {
+      const params = this._getParams(args);
+      const tx = params[0];
+
+      // let's pretend that EIP-1559 never happened
+      tx.gasPrice = this.gasPrice;
+    }
+
+    return this._wrappedProvider.request(args);
+  }
+}
+
+extendProvider(async (provider, config, network) => {
+  // We fix the gas price to be set by the config or to a random high value
+  const gasPrice = (config as HardhatConfig & { fixedGasPrice: bigint }).fixedGasPrice || BigInt("0x1000000")
+  const newProvider = new FixedGasProvider(gasPrice, provider);
+  return newProvider;
+});
+
+task("request", async (args, hre) => {
+  await hre.network.provider.request({ method: "eth_estimateGas" }); // this will run `FixedGasProvider` request method above
 });
 
 const config: HardhatUserConfig = {
